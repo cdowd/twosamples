3b36662eefa3a4ca71f8128b71f8e51e09c14840
cdowd <cdowd@chicagobooth.edu>
2022-07-09 13:13:09 -0400

    bettter tests

--- tests/testthat/test-two_samples.R
+++ tests/testthat/test-two_samples.R
@@ -5,6 +5,11 @@ test_that("permutation_builder outputs a function", {
   expect_type(permutation_test_builder(f),"closure")
 })
 
+test_that("test_stat_builder outputs a function", {
+  f = function(vec1,vec2,p=2) (mean(vec1^p)-mean(vec2^p))^(1/p)
+  expect_type(stat_fn_builder(f),"closure")
+})
+
 # stats calculations are correct
 test_that("stat calculations are correct",{
   vec1 = c(0,2,3)
@@ -58,6 +63,30 @@ test_that("power against obvious null rejection",{
   expect_lt(ad_test(vec1,vec2)[2],0.05)
   expect_lt(wass_test(vec1,vec2)[2],0.05)
   expect_lt(dts_test(vec1,vec2)[2],0.05)
+})
+
+#Class functions are quasi functional
+test_that("Classes are working",{
+  vec1 = rnorm(100)
+  vec2 = rnorm(100,0.2)
+  out  = dts_test(vec1,vec2,keep.samples = T)
+  out2 = dts_test(vec1,vec2)
+  expect_s3_class(out,"twosamples")
+  expect_length(attr(out,"bootstraps"),2000)
+  expect_equal(attr(out,"samples"),list(a=vec1,b=vec2))
+  expect_null(attr(out2,"samples"))
+  comb = combine.twosamples(out,out2)
+  expect_s3_class(comb,"twosamples")
+  expect_length(attr(comb,"bootstraps"),4000)
+  expect_equal(unname(attr(comb,"details")[3]),4000)
+  expect_equal(comb[2],(out[2]+out2[2])/2)
+  out3 = ks_test(vec1,vec2)
+  expect_error(combine.twosamples(out,out3))
+  out4 = dts_test(vec1,vec1)
+  expect_error(combine.twosamples(out,out4))
+  out5 = dts_test(vec1,vec2,keep.boots=F)
+  expect_null(attr(out5,"bootstraps"))
+  expect_error(combine.twosamples(out,out5))
 })
 
 
