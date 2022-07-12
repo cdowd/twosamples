1e305c0682d4a385f496ebeb044da13767e6f73f
cdowd <cdowd@chicagobooth.edu>
2022-07-11 22:20:44 -0400

    seed documentation examples, reduce gap in mean x values

--- R/documentation.R
+++ R/documentation.R
@@ -25,8 +25,9 @@ NULL
 #' Inputs `a` and `b` can also be vectors of ordered (or unordered) factors, so long as both have the same levels and orderings. When possible, ordering factors will substantially increase power.
 #' @seealso [dts_test()] for a more powerful test statistic. See [kuiper_test()] or [cvm_test()] for the natural successors to this test statistic.
 #' @examples
+#' set.seed(314159)
 #' vec1 = rnorm(20)
-#' vec2 = rnorm(20,4)
+#' vec2 = rnorm(20,0.5)
 #' out = ks_test(vec1,vec2)
 #' out
 #' summary(out)
@@ -61,8 +62,9 @@ NULL
 #' Inputs `a` and `b` can also be vectors of ordered (or unordered) factors, so long as both have the same levels and orderings. When possible, ordering factors will substantially increase power.
 #' @seealso [dts_test()] for a more powerful test statistic. See [ks_test()] for the predecessor to this test statistic, and [cvm_test()] for its natural successor.
 #' @examples
+#' set.seed(314159)
 #' vec1 = rnorm(20)
-#' vec2 = rnorm(20,4)
+#' vec2 = rnorm(20,0.5)
 #' out = kuiper_test(vec1,vec2)
 #' out
 #' summary(out)
@@ -98,8 +100,9 @@ NULL
 #' Inputs `a` and `b` can also be vectors of ordered (or unordered) factors, so long as both have the same levels and orderings. When possible, ordering factors will substantially increase power.
 #' @seealso [dts_test()] for a more powerful test statistic. See [ks_test()] or [kuiper_test()] for the predecessors to this test statistic. See [wass_test()] and [ad_test()] for the successors to this test statistic.
 #' @examples
+#' set.seed(314159)
 #' vec1 = rnorm(20)
-#' vec2 = rnorm(20,4)
+#' vec2 = rnorm(20,0.5)
 #' out = cvm_test(vec1,vec2)
 #' out
 #' summary(out)
@@ -139,8 +142,9 @@ NULL
 #' Inputs `a` and `b` can also be vectors of ordered (or unordered) factors, so long as both have the same levels and orderings. When possible, ordering factors will substantially increase power.
 #' @seealso [dts_test()] for a more powerful test statistic. See [cvm_test()] for the predecessor to this test statistic. See [dts_test()] for the natural successor to this test statistic.
 #' @examples
+#' set.seed(314159)
 #' vec1 = rnorm(20)
-#' vec2 = rnorm(20,4)
+#' vec2 = rnorm(20,0.5)
 #' out = ad_test(vec1,vec2)
 #' out
 #' summary(out)
@@ -175,8 +179,9 @@ NULL
 #' Inputs `a` and `b` can also be vectors of ordered (or unordered) factors, so long as both have the same levels and orderings. When possible, ordering factors will substantially increase power. `wass_test` will assume the distance between adjacent factors is 1.
 #' @seealso [dts_test()] for a more powerful test statistic. See [cvm_test()] for the predecessor to this test statistic. See [dts_test()] for the natural successor of this test statistic.
 #' @examples
+#' set.seed(314159)
 #' vec1 = rnorm(20)
-#' vec2 = rnorm(20,4)
+#' vec2 = rnorm(20,0.5)
 #' out = wass_test(vec1,vec2)
 #' out
 #' summary(out)
@@ -215,8 +220,9 @@ NULL
 #' Inputs `a` and `b` can also be vectors of ordered (or unordered) factors, so long as both have the same levels and orderings. When possible, ordering factors will substantially increase power. The dts test will assume the distance between adjacent factors is 1.
 #' @seealso [wass_test()], [ad_test()] for the predecessors of this test statistic. [arXiv:2007.01360](https://arxiv.org/abs/2007.01360) or <https://codowd.com/public/DTS.pdf> for details of this test statistic
 #' @examples
+#' set.seed(314159)
 #' vec1 = rnorm(20)
-#' vec2 = rnorm(20,4)
+#' vec2 = rnorm(20,0.5)
 #' dts_stat(vec1,vec2)
 #' out = dts_test(vec1,vec2)
 #' out
@@ -264,8 +270,9 @@ NULL
 #' @examples
 #' mean_stat = function(joint,label,p,na) abs(mean(joint[label])-mean(joint[!label]))**p
 #' myfun = twosamples:::permutation_test_builder(mean_stat,2.0)
+#' set.seed(314159)
 #' vec1 = rnorm(20)
-#' vec2 = rnorm(20,4)
+#' vec2 = rnorm(20,0.5)
 #' out = myfun(vec1,vec2)
 #' out
 #' summary(out)
